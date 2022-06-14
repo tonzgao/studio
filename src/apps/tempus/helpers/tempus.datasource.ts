ddd6394a16fde6d596fcd970648ca37251015cf3
TonyG <tonyzgao@gmail.com>
2022-06-14 13:08:45 +0200

    fix: tempus - address project comments

--- src/apps/tempus/helpers/tempus.datasource.ts
+++ src/apps/tempus/helpers/tempus.datasource.ts
@@ -1,8 +1,11 @@
 import Axios from 'axios';
+import _ from 'lodash';
 
 import { Network } from '~types';
 
-const datasourceUrl = 'https://raw.githubusercontent.com/tempus-finance/tempus-pools-config/master/config.json';
+const datasourceUrl =
+  'https://raw.githubusercontent.com/tempus-finance/tempus-app/release-3.0/packages/tempus-client_v3/src/config/config.ts';
+const start = 'Config =';
 
 export type TempusPool = {
   address: string;
@@ -17,7 +20,39 @@ type TempusData = {
 };
 type TempusDataResponse = Record<Network, TempusData>;
 
+// TODO: find JSON to load instead
+const parseConfig = (config: string) => {
+  config = config.slice(config.search(start) + start.length);
+  const result = {};
+  let current = {};
+  let level = 0;
+  let network = '';
+
+  for (const line of config.split('\n')) {
+    if (line.includes('}') && !line.includes('{')) {
+      level--;
+    } else if (level === 3) {
+      const [key, value] = line.trim().split(': ');
+      current[key] = value.replaceAll("'", '').replace(',', '');
+    }
+    if (line.includes('{') && !line.includes('}')) {
+      if (level === 1 || level === 2) {
+        if (_.keys(current).length) result[network].tempusPools.push(current);
+        current = {};
+      }
+      if (level === 1) {
+        network = line.trim().split(': ')[0];
+        result[network] = { tempusPools: [] };
+      }
+      level++;
+    }
+  }
+  return result;
+};
+
 export const getTempusData = async (network: string) => {
-  const pools = (await Axios.get<TempusDataResponse>(datasourceUrl)).data[network];
+  const data = (await Axios.get<string>(datasourceUrl)).data;
+  const json = parseConfig(data) as TempusDataResponse;
+  const pools = json[network];
   return pools as TempusData;
 };
